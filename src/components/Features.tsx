39399fec891f3f34f0daad5f26aa412caa88bb20
Saif Ali <90478821+isaifalikhan@users.noreply.github.com>
2025-10-01 01:39:20 +0500

    fix

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -2,7 +2,7 @@
 
 export default function Features() {
   return (
-    <section className="py-12 sm:py-16 lg:py-20 px-4 sm:px-6 lg:px-8 mx-auto" style={{ maxWidth: '1570px', width: '100%' }}>
+    <section className="py-12 sm:py-16 lg:py-20  mx-auto" style={{ maxWidth: '1570px', width: '100%' }}>
       <div className="w-full">
         {/* Sleek Design */}
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 sm:gap-12 lg:gap-16 border-b border-gray-600 pb-6 sm:pb-8 mb-6 sm:mb-8">
